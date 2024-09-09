166f499adf8271e601d255fbe4fe77733d772f09
gengteng <me@gteng.org>
2024-09-09 16:58:16 +0800

    feat: add index route

--- backend/crates/server/src/route.rs
+++ backend/crates/server/src/route.rs
@@ -1,14 +1,17 @@
 //! Routes for the Music3 backend
 //!
 
+use crate::auth::claim::Claim;
 use crate::conf::Config;
-use axum::routing::post;
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
 use axum::Router;
 
 /// Music3 backend routes
 pub fn router(config: Config) -> anyhow::Result<Router> {
     let authorizer = crate::auth::Authorizer::new(config.auth)?;
     let router = Router::new()
+        .route("/", get(index))
         .nest(
             "/auth",
             Router::new()
@@ -17,4 +20,12 @@ pub fn router(config: Config) -> anyhow::Result<Router> {
         )
         .with_state(authorizer);
     Ok(router)
+}
+
+/// Index route
+pub async fn index(claim: Option<Claim>) -> impl IntoResponse {
+    match claim {
+        Some(claim) => format!("Hello, {}!", claim.sub),
+        None => "Hello, guest!".to_string(),
+    }
 }
